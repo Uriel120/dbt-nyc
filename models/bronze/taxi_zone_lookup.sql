93e2922eb62f4c43190b483294cea94373d97052
Toffe Gokale Michel <michel.toffe@artefact.com>
2025-10-26 02:45:48 +0000

    update bronze layer schema and add another source

--- models/bronze/taxi_zone_lookup.sql
+++ models/bronze/taxi_zone_lookup.sql
@@ -1,2 +1,3 @@
-select * 
+select *,
+current_datetime() as load_date
 from {{ source('nycdata', 'taxi_zone_lookup') }}
--- models/bronze/yellow_nyc_data.sql
+++ models/bronze/yellow_nyc_data.sql
@@ -1,2 +1,3 @@
-select *
+select *,
+current_datetime() as load_date
 from {{ source('nycdata', 'yellow_nyc_data') }}
